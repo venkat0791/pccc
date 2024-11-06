15b6c310408c2ebd54d6120de6080054feef698a
Venkat Venkatesan <sivarama.venkatesan@gmail.com>
2024-11-06 18:00:15 -0500

    Use "turbo iterations" consistently in docs
    This is in line with the renaming of the CLI parameter to
    num_turbo_iter.

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,14 @@
 //!
 //! The encoder for a PCCC comprises the parallel concatenation of two identical _recursive
 //! systematic convolutional_ (RSC) encoders, separated by an internal interleaver. The decoder is
-//! based on iterations between two corresponding soft-input/soft-output _a posteriori probability_
-//! (APP) decoders, separated by an interleaver and deinterleaver.
+//! based on "turbo" iterations between two corresponding soft-input/soft-output _a posteriori
+//! probability_ (APP) decoders, separated by an interleaver and deinterleaver.
 //!
 //! The [`encoder`] and [`decoder`] functions handle PCCC encoding and decoding, respectively,
 //! while the [`Interleaver`] struct models the internal interleaver and associated deinterleaver.
-//! The [`Bit`] enum represents binary symbol values, and the [`DecodingAlgo`] enum lists the
-//! supported decoding algorithms; each variant of the latter holds a [`u32`] representing the
-//! number of turbo iterations. The code below illustrates their usage through a toy example.
+//! The [`Bit`] enum represents binary symbol values, and the [`DecodingAlgo`] enum the supported
+//! decoding algorithms; each variant of the latter holds a [`u32`] representing the number of
+//! turbo iterations. The code below illustrates their usage through a toy example.
 //!
 //! # Examples
 //!
@@ -23,9 +23,10 @@
 //! use pccc::{decoder, encoder, Bit, DecodingAlgo, Interleaver};
 //! use Bit::{One, Zero};
 //!
-//! // Rate-1/3 PCCC in LTE, 4 information bits, Log-MAP decoding (8 iterations)
+//! // Rate-1/3 PCCC in LTE, 4 information bits, Log-MAP decoding (8 turbo iterations)
 //! let code_polynomials = [0o13, 0o15];
 //! let interleaver = Interleaver::new(&[3, 0, 1, 2])?;
+//!
 //! // Encoding
 //! let info_bits = [One, Zero, Zero, One];
 //! let code_bits = encoder(&info_bits, &interleaver, &code_polynomials)?;
@@ -36,6 +37,7 @@
 //!         Zero, Zero, Zero, One, One, One, Zero, Zero
 //!     ]
 //! );
+//!
 //! // Decoding
 //! let code_bits_llr = [
 //!     -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
@@ -54,9 +56,10 @@
 //! The [`lte`] module contains encoder and decoder functions specifically for the rate-1/3 PCCC
 //! used in the LTE standard; the block size is restricted to the values in Table 5.1.3-3 of 3GPP
 //! TS 36.212 (from 40 to 6144 bits), and the interleaver for each of these block sizes is
-//! automatically set to the prescribed _quadratic permutation polynomial_ (QPP) interleaver. This
-//! module also has functions to evaluate the performance of the LTE PCCC over a BPSK-AWGN channel
-//! by simulation. Finally, the [`utils`] module has some useful functions for such a simulation.
+//! automatically set to the _quadratic permutation polynomial_ (QPP) interleaver prescribed
+//! therein. This module also has functions to evaluate the performance of the LTE PCCC over a
+//! BPSK-AWGN channel by simulation. Finally, the [`utils`] module has some useful functions for
+//! such a simulation.
 
 #![warn(
     clippy::complexity,
