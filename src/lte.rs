c4a9378940c9f6928d5532bce85ccc5d45b0a94d
Venkat Venkatesan <sivarama.venkatesan@gmail.com>
2024-11-08 06:20:28 -0500

    Save results to file after each simulation case
    In lte::run_bpsk_awgn_sims, moved the call to the function saving
    all simulation results to file inside the loop over all cases.
    This means the whole set of results up to that point is saved
    again after each case is completed. This is a bit redundant, but
    it is probably better than saving everything only at the end.
    
    What would be best is saving incrementally to the file after each
    case is completed.

--- src/lte.rs
+++ src/lte.rs
@@ -397,12 +397,12 @@ pub fn run_bpsk_awgn_sims(
         params.print();
         if let Ok(results) = bpsk_awgn_sim(params, rng) {
             all_results.push(results);
+            save_all_sim_results_to_file(&all_results, json_filename)?;
         } else {
             eprintln!("WARNING: Invalid simulation parameters");
         }
     }
     summarize_all_sim_results(&all_results);
-    save_all_sim_results_to_file(&all_results, json_filename)?;
     Ok(())
 }
 
